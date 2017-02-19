d1336c2fbfd831d517e4598e4e8873697e1106f6
Farhad Ahmed <farhad.ahmed@fahmed02-ml.local>
2017-02-18 16:51:48 -0800

    Edit intro lesson 12

--- day03-JavaScript/18-type-coercion.js
+++ day03-JavaScript/18-type-coercion.js
@@ -1,7 +1,10 @@
 // Data Type Conversion
 
 'Farhad' + 5
+// 'Farhad5'
 
 '5' - 1
+// 4
 
 'Farhad' * 5
+// NaN
---
+++ day03-JavaScript/19B-variables.js
@@ -0,0 +1,18 @@
+var fname;
+
+
+var name = 'Adam';
+
+name = 'John';
+
+
+var basesalary = 50000
+var tax = basesalary / 15;
+var netsalary = basesalary - tax;
+
+console.log(netsalary);
+// 46666.666666666664
+
+
+// Create variables concatonating other variables
+// Create variables involving a math equation
